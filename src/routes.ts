f01644a757e1ce1d72ea7cf2109a68148add7b09
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2021-08-06 19:17:46 +0530

    👦 create user

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,20 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validateRequest from "./middleware/validateRequest";
+import { createUserSchema } from "./schema/user.schema";
 
 export default function (app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+
+  // Register user
+  app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
+
+  // Login
+  // POST /api/sessions
+
+  // Get the user's sessions
+  // GET /api/sessions
+
+  // Logout
+  // DELETE /api/sessions
 }
